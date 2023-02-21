62bd248591f6a8bb59a06d0a8698cf5074f97e42
Ahmad Bilal Farooqi <ahmadbilal20152016@gmail.com>
2023-02-21 23:04:53 +0500

    new

---
+++ q15.ts
@@ -0,0 +1,10 @@
+let guestperson1 = ["Rao_Safdar", "Javed_Khan_Lashari", "Dilawar", "Imran"]
+//console.log(guestperson1);
+
+guestperson1.pop();
+console.log(guestperson1);
+console.log("Adil will not come to dinner");
+
+console.log('hi, '+guestperson1[0]+ " would you like to invite a dinner?");
+console.log('hi, '+guestperson1[1]+ " would you like to invite a dinner?");
+console.log('hi, '+guestperson1[2]+ " would you like to invite a dinner?");
